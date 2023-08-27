fc940d3fb35dd909061440d97d593a3599067894
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-26 22:20:45 -0500

    feat: :sparkles: Variables
    Se crea un archivo en donde se importan las variables de entorno y se exportan a los archivos que necesiten esas varibales
    
    by: Jhon Hernandez

---
+++ src/config/variables.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const my_config = JSON.parse(process.env.MY_CONFIG)
+export const my_connect = JSON.parse(process.env.MY_CONNECT)
+export const my_jwt = process.env.MY_JWT
